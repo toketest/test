b7fd03c0ccec9167a0203a80e30f69969ce008c7
sdfs <84703325+toketest@users.noreply.github.com>
2023-05-10 17:53:46 +0200

    Add files via upload

---
+++ 1_Homepage.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
